de03b518df7e135a06a64290442703f3df2c66d1
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2026-04-07 23:29:03 -0400

    feat: Nave y funcionalidad para el giro

---
+++ main.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById("canva");
+const lapiz  = canvas.getContext('2d');
+canvas.width  = 1500;
+canvas.height = 700;
+
+const navesita = {
+  x: canvas.width/2,
+  y: canvas.height/2, 
+  angulo: Math.PI/2,
+  velocidadGiro: 0.07 
+};
+
+const teclas = {};
+document.addEventListener('keydown', e => teclas[e.key] = 1);
+document.addEventListener('keyup',   e => teclas[e.key] = 0);
+
+function dibujar() {
+  lapiz.fillStyle = '#000';
+  lapiz.fillRect(0, 0, canvas.width, canvas.height);
+  lapiz.save();
+
+
+  lapiz.translate(navesita.x, navesita.y);
+  lapiz.rotate(navesita.angulo);  
+
+  lapiz.fillStyle = '#f00';
+  lapiz.font = '48px monospace';
+  lapiz.textAlign = 'center';
+  lapiz.textBaseline = 'middle';
+  lapiz.fillText('A', 0, 0);
+  lapiz.restore();
+}
+
+
+function juego(){
+  if(teclas['ArrowLeft']){
+    navesita.angulo -= navesita.velocidadGiro;
+  }
+
+  if(teclas['ArrowRight']){ 
+    navesita.angulo += navesita.velocidadGiro;
+  }
+
+  dibujar();
+  setTimeout(juego, 16);
+}
+
+juego();
+
+// function gameLoop() {
+//   
+//   if (teclas['ArrowLeft'])  navesita.angulo -= navesita.velocidadGiro;
+//   if (teclas['ArrowRight']) navesita.angulo += navesita.velocidadGiro;
+//   dibujar();
+//   requestAnimationFrame(gameLoop);
+// }
+
+// requestAnimationFrame(gameLoop);
